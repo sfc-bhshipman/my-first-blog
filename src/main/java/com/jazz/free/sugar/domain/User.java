19edcb40f4a661f6fd4e25bad575bdab8b6863fd
bshipman <bshipman@imcc.com>
2021-02-10 21:19:20 -0600

    update entity schema

--- src/main/java/com/jazz/free/sugar/domain/User.java
+++ src/main/java/com/jazz/free/sugar/domain/User.java
@@ -23,7 +23,7 @@ import java.util.Set;
  * A user.
  */
 @Entity
-@Table(name = "jhi_user")
+@Table(name = "jhi_user", schema = "public")
 @Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
 public class User extends AbstractAuditingEntity implements Serializable {
 
